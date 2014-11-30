4b208f24e68a1ac86584e702488946b590fc4c6a
kahlow <tkahlow@gmail.com>
2014-11-30 09:02:14 -0500

    updates

--- conditions/homework/practice.php
+++ conditions/homework/practice.php
@@ -1,6 +1,15 @@
 <?php
-    /*
-        Write a program that 
-    */
+/*
+    Using the variables I created below write both an if statement and a switch statement 
+    to print out the phrase "My doctor says I can't have too much sodium."
+
+    In the if statement, you have to use else-if and else. The else condition should print out that phrase above.
+
+    In the switch statement, have at least 4 cases. Have the phrase print on the default case.
+*/
+
+$num = 5;
+
+
 
 ?>
